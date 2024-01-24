ec5916da22ea99e3e21e184c6716fb7999dbf13b
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-01-24 23:40:14 +0900

    編集ボタンの追加

--- app/components/memo/List.tsx
+++ app/components/memo/List.tsx
@@ -1,5 +1,5 @@
 import prisma from "../../action/prisma";
-import { deleteMemo } from "../../action/action-memo";
+import { deleteMemo, updateMemo } from "../../action/action-memo";
 import Button from "../button/Button";
 
 const List = async () => {
@@ -13,7 +13,13 @@ const List = async () => {
                     <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-80" key={memo.id} >
                         <div className="border-b-2">{memo.name}</div>
                         <div className="form-control textarea">{memo.content}</div>
-                        <button>保存</button>
+                        <form action={updateMemo}>
+                        <input type="text" name="name" defaultValue={memo?.name} /> 
+                        <input type="text" name="content" defaultValue={memo?.content} />   
+                        <Button>
+                            編集
+                        </Button>
+                        </form>
                         <form action={deleteMemo}>
                         <input type="hidden" name="id" value={memo.id} />  
                         <Button>

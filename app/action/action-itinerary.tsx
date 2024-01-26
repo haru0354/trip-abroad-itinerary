9d63d4423144ac38d3a31f4494c365c80ab1ed5e
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-01-26 15:14:58 +0900

    旅程表の追加ボタンをアクションにより実装

--- app/action/action-itinerary.tsx
+++ app/action/action-itinerary.tsx
@@ -2,6 +2,16 @@
 
 import { revalidatePath } from "next/cache";
 
+export const addItinerary = async (data: FormData) => {
+  const date = data.get("date") as string;
+  const time = data.get("time") as string;
+  const name = data.get("name") as string;
+  const content = data.get("content") as string;
+  const hideContent = data.get("hideContent") as string;
+  await prisma.itinerary.create({ data: { date, time, name, content, hideContent } })
+  revalidatePath('/itinerary');
+}
+
 export const deleteItinerary = async (data: FormData) => {
     const id = data.get('id') as string;
     await prisma.itinerary.delete({
--- app/itinerary/page.tsx
+++ app/itinerary/page.tsx
@@ -3,16 +3,23 @@ import Form from "../components/Form";
 import Textarea from "../components/Textarea";
 import Date from "../components/Date";
 import Time from "../components/Time";
+import Button from "../components/Button";
+import { addItinerary } from "../action/action-itinerary";
 
 const page = () => {
   return (
     <main>
       <div>
+        <form action={addItinerary}>
         <Date />
         <Time />
         <Form label={"目的"} placeholder={"飛行機・食事・観光など"} name={"name"}  />
         <Textarea label={"補足情報"} placeholder={"観光地なら服装の注意。料理ならおすすめの料理などメモを記載しましょう。"} name={"content"}  />
-        <Textarea label={"補足情報2"} placeholder={"ここに記載したのは最初は非常時でボタンをクリックで表示されます。旅程表とは関係のない観光地の情報や乗り換え方法など記載しましょう。"} name={"isShowContent"}  />
+        <Textarea label={"補足情報2"} placeholder={"ここに記載したのは最初は非常時でボタンをクリックで表示されます。旅程表とは関係のない観光地の情報や乗り換え方法など記載しましょう。"} name={"hideContent"}  />
+        <Button>
+          追加
+        </Button>
+        </form>
       </div>
       <ItineraryList />
 

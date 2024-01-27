56ae42ac8e327df4b452e9bd8febf7bf6d8611e8
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-01-27 23:08:50 +0900

    旅程表の補足情報の表示と非表示の切り替えを実装

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -7,15 +7,20 @@ import { useFormStatus } from 'react-dom';
 type ButtonChildren = {
     children: ReactNode;
     formAction?: (data: FormData) => Promise<void> | Promise<never>;
+    onClick?: () => void; 
 }
 
-const Button: React.FC<ButtonChildren> = ({ children, formAction }) => {
+const Button: React.FC<ButtonChildren> = ({ children, formAction, onClick }) => {
     const { pending } = useFormStatus();
 
     return (
         <button
-            className={`bg-blue-600 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ${pending ? 'opacity-100' : ''}`}
-            type="submit" formAction={formAction} disabled={pending} >
+            className={`bg-blue-600 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ${pending ? 'opacity-30' : ''}`}
+            type="submit"
+            formAction={formAction}
+            disabled={pending}
+            onClick={onClick}
+        >
             {children}
         </button>
     );

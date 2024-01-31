68efa95a1aee977c738944a9cf5cdb480921e55f
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-01-31 22:16:25 +0900

    ラベルのhtmlForを設定

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -9,10 +9,11 @@ type FormProps = {
 const Form: React.FC<FormProps> = ({ name, label, defaultValue, placeholder }) => {
     return (
         <div>
-            <label className="block text-gray-600 text-sm font-bold mb-2">{label}</label>
+            <label className="block text-gray-600 text-sm font-bold mb-2" htmlFor={label} >{label}</label>
             <input 
                 type="text" 
                 className="shadow border rounded w-full py-2 px-3"
+                id={label}
                 name={name} 
                 defaultValue={defaultValue} 
                 placeholder={placeholder}/>

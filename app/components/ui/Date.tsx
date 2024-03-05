828e47450f89df6819e1b921b66ddefc64d5090d
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-03-05 23:42:50 +0900

    アプリにhomeを追加・各旅行がmemoを持てるように変更

--- app/components/ui/Date.tsx
+++ app/components/ui/Date.tsx
@@ -1,20 +1,35 @@
 type DateProps = {
   name: string;
   value?: string;
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   defaultValue?: string;
+  label?: string;
 };
 
-const Date: React.FC<DateProps> = ({ name, value, onChange, defaultValue }) => {
+const Date: React.FC<DateProps> = ({
+  name,
+  value,
+  onChange,
+  defaultValue,
+  label = "日付:",
+}) => {
   return (
     <div>
       <label
         className="block text-gray-600 text-sm font-bold  mb-1 mt-4"
-        htmlFor="date"
+        htmlFor={label}
       >
-        日付:
+        {label}
       </label>
-      <input type="date" id="date" name={name} value={value} defaultValue={defaultValue} onChange={onChange} className="shadow border rounded py-2 px-2" />
+      <input
+        type="date"
+        id={label}
+        name={name}
+        value={value}
+        defaultValue={defaultValue}
+        onChange={onChange}
+        className="shadow border rounded py-2 px-2"
+      />
     </div>
   );
 };
---
+++ app/travel_brochure/home/page.tsx
@@ -0,0 +1,20 @@
+import FormItineraryHome from "@/app/components/itineraryHome/FormItineraryHome"
+import ListItineraryHome from "@/app/components/itineraryHome/ListItineraryHome"
+import getCurrentUser from "@/app/action/getCurrentUser";
+import { addItineraryHome } from "@/app/action/action-ItineraryHome";
+
+const page = async () => {
+  const currentUser = await getCurrentUser();
+  const userId = currentUser?.id
+
+  return (
+    <div>
+      <FormItineraryHome buttonName="追加" userId={userId} formAction={addItineraryHome}/>
+      <ListItineraryHome userId={userId}/>
+
+
+    </div>
+  )
+}
+
+export default page

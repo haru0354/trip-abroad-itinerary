79237a71ba47059a498bf4ee4ef23b473f9d7c56
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-03-07 15:42:59 +0900

    アプリのTOPページの画像以外の部分を作成

--- app/components/homePage/Section1ColumnRight.tsx
+++ app/components/homePage/Section1ColumnRight.tsx
@@ -19,7 +19,10 @@ const Section1ColumnRight: React.FC<Section1ColumnRightProps> = ({
 }) => {
   return (
     <>
-      <div className="flex justify-center  w-full py-8  rounded flex-wrap">
+      <h3 className="text-gray-700 mb-6 py-2 text-2xl font-semibold border-b border-gray-600 text-center">
+        {name}
+      </h3>
+      <div className="flex justify-center  w-full py-4  rounded flex-wrap">
         <div className="w-full flex justify-center items-center py-4  max-w-[400px] ">
           <Image
             src={src}
@@ -27,13 +30,12 @@ const Section1ColumnRight: React.FC<Section1ColumnRightProps> = ({
             width={350}
             height={253}
             style={{
-              width: '350px',
-              height: 'auto',
+              width: "350px",
+              height: "auto",
             }}
           />
         </div>
         <div className="w-full py-4 mx-14 max-w-[400px] justify-center items-center">
-          <h3 className="text-gray-700 mb-6 text-2xl font-semibold ">{name}</h3>
           <p className="text-gray-700 mb-6">{content}</p>
           <p className="text-gray-700 mb-6">{content2}</p>
           <p className="text-gray-700 mb-6">{content3}</p>

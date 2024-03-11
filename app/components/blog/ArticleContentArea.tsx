0184b4c2eb462be10eb91cc4dc4e7d70e5ed260e
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-03-11 18:10:33 +0900

    クライアントへの表示時にサニタイズを実装

--- app/components/blog/ArticleContentArea.tsx
+++ app/components/blog/ArticleContentArea.tsx
@@ -1,12 +1,21 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import parse from "html-react-parser";
+import DOMPurify from "dompurify";
 
 type ArticleContentAreaProps = {
   content: string;
 };
 
 const ArticleContentArea: React.FC<ArticleContentAreaProps> = ({ content }) => {
+  const [sanitizedContent, setSanitizedContent] = useState("");
 
-  return <>{parse(content)}</>;
+  useEffect(() => {
+    const sanitized = DOMPurify.sanitize(content);
+    setSanitizedContent(sanitized);
+  }, [content]);
+
+  return <>{parse(sanitizedContent)}</>;
 };
 export default ArticleContentArea;

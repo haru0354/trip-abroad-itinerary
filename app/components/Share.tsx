ae52c7430e0237c211b6a32d2f0fa77c5f8e7aad
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-03-13 15:24:07 +0900

    共有ページにボタンクリックでURLのコピーを設置

--- app/components/Share.tsx
+++ app/components/Share.tsx
@@ -1,14 +1,39 @@
 "use client";
 
 import { usePathname } from "next/navigation";
+import { useState } from "react";
+import Button from "./ui/Button";
 
 const Share = () => {
   const path = usePathname();
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(path);
+    setCopied(true);
+  };
 
   return (
-    <div className="p-4 my-8 mx-16 border border-dashed border-gray-500">
-      URL:〇〇〇{path}
-    </div>
+    <>
+      <div className="p-4 mt-20 mb-8 mx-10 border border-dashed border-gray-500">
+        <div className="block mx-auto">
+          <p className="p-4 mx-auto bg-gray-200 border-b border-gray-500 mb-6">
+            {path}
+          </p>
+          <Button
+            onClick={handleCopy}
+            size="small"
+            color="blue"
+            className="rounded"
+          >
+            URLをコピー
+          </Button>
+          {copied && <p className="text-green-600 ml-3">(コピーされました)</p>}
+          <p className="mt-4">この旅程表をSNSやLINEで共有しましょう。</p>
+          <p>また、旅行の同行者と旅程表を共有することもできます。</p>
+        </div>
+      </div>
+    </>
   );
 };
 

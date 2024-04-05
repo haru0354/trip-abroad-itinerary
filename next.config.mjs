d9a212a9db93dff90034517bb888e053a021cb58
haru0354 <155523058+haru0354@users.noreply.github.com>
2024-04-05 22:06:18 +0900

    supabaseに画像を追加できるように変更

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,15 @@
 const nextConfig = {
   env: {
     DOTENV_LOCAL_PATH: '.env.local'
-  }
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ybfrmhradztqizzobkvg.supabase.co',
+      },
+    ],
+  },
 };
   
 export default nextConfig;

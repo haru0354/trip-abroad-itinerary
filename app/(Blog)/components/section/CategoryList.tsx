e3ade29e1c578b2dd73a9a68b0105c697c04cad7
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-01-03 13:29:48 +0900

    categoryTopをcategoryListに名称変更しblogのsectionに移動し修正

---
+++ app/(Blog)/components/section/CategoryList.tsx
@@ -0,0 +1,96 @@
+import Link from "next/link";
+import Image from "next/image";
+import { getCategories } from "@/app/(blog)/lib/service/blogServiceMany";
+import AnimatedItem from "@/app/lib/animation/AnimatedItem";
+import Section from "@/app/components/1ColumnPage/Section";
+
+const CategoryList = async () => {
+  const categories = await getCategories("postsAndPostImage");
+
+  return (
+    <Section bgColor="bg-white" name="カテゴリ">
+      <div className="flex w-full my-8 flex-wrap items-center justify-center">
+        {categories.map((category) => {
+          if (
+            !category ||
+            ((!category.title || category.title === "") &&
+              category.posts.every((post) => !post.draft))
+          ) {
+            return null;
+          }
+          return (
+            <AnimatedItem
+              elementType="div"
+              animation="fadeInVariants"
+              key={category.id}
+              className="flex flex-col items-center"
+            >
+              <Link href={`/${category.slug}`}>
+                <div className=" rounded mx-5 my-6 flex flex-col items-center  max-w-[330px] min-w-[330px]">
+                  <div className="min-h-[222px] max-h-[222px] border justify-centers">
+                    {category.postImage ? (
+                      <figure
+                        style={{
+                          position: "relative",
+                          width: "330px",
+                          height: "220px",
+                        }}
+                      >
+                        <Image
+                          src={category.postImage.url}
+                          alt={category.postImage.altText}
+                          fill
+                          sizes="(max-hight: 220px)"
+                          style={{
+                            objectFit: "cover",
+                          }}
+                        />
+                      </figure>
+                    ) : (
+                      <figure
+                        style={{
+                          position: "relative",
+                          width: "330px",
+                          height: "220px",
+                        }}
+                      >
+                        <Image
+                          src="/no_image.jpg"
+                          alt="画像の準備中"
+                          fill
+                          sizes="(max-hight: 220px)"
+                          style={{
+                            objectFit: "cover",
+                          }}
+                        />
+                      </figure>
+                    )}
+                  </div>
+                  <div className="px-3 py-1 min-h-[180px]">
+                    <h3 className="text-gray-700 my-6 text-center text-xl font-semibold">
+                      {category.name && category.name.length > 12 ? (
+                        <>{category.name.slice(0, 12)}...</>
+                      ) : (
+                        <>{category.name}</>
+                      )}
+                    </h3>
+                    <p className="text-gray-600 my-2">
+                      {category.description &&
+                      category.description.length > 56 ? (
+                        <>{category.description.slice(0, 56)}...</>
+                      ) : (
+                        <>{category.description}</>
+                      )}
+                    </p>
+                  </div>
+                </div>
+              </Link>
+            </AnimatedItem>
+          );
+        })}
+      </div>
+    </Section>
+  );
+};
+
+export default CategoryList;
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Header from "./(blog)/components/Header";
 import Section from "./components/1ColumnPage/Section";
 import Section1ColumnRight from "./components/1ColumnPage/Section1ColumnRight";
-import CategoryTop from "./components/1ColumnPage/CategoryTop";
+import CategoryList from "./(blog)/components/section/CategoryList";
 import NewArticleTop from "./components/1ColumnPage/NewArticleTop";
 import Footer from "./(blog)/components/Footer";
 import Button from "./components/ui/Button";
@@ -15,7 +15,7 @@ export default function Home() {
       <HeaderImage />
       <main>
         <div className="w-full">
-        <section className="bg-white">
+          <section className="bg-white">
             <div className="max-w-[1150px] w-full py-2 md:py-6 px-4 mx-auto">
               <div className="flex items-center mx-0 py-8">
                 <span className="flex-grow h-1 w-5 md:w-0 mr-1 md:mr-4 bg-gradient-to-l from-gray-600 to-transparent"></span>
@@ -24,28 +24,26 @@ export default function Home() {
                 </h2>
                 <span className="flex-grow h-1 w-5 md:w-0 mr-1 md:mr-4 bg-gradient-to-r from-gray-600 to-transparent"></span>
               </div>
-            <Section1ColumnRight
-              src="/travel_memory_thumbnail.jpg"
-              alt="旅のメモリーブックのサムネイル"
-              name="「旅のメモリーブック」"
-              content="PC・スマホ・タブレットで利用できる無料の国内旅行・海外旅行しおりアプリ。"
-              content2="簡単に旅程表を作成でき、旅行中に撮影した写真を掲載し、旅の思い出のしおりして残すことができます。"
-              content3="特に「英語が話せない人の海外旅行」向けに事前にメモできる項目が多く設定されています。"
-              isPriority={true}
-            />
-            <Link href="./memorybook/">
-              <Button color="blue" size="normal">
-                旅のメモリーブックへ
-              </Button>
-            </Link>
+              <Section1ColumnRight
+                src="/travel_memory_thumbnail.jpg"
+                alt="旅のメモリーブックのサムネイル"
+                name="「旅のメモリーブック」"
+                content="PC・スマホ・タブレットで利用できる無料の国内旅行・海外旅行しおりアプリ。"
+                content2="簡単に旅程表を作成でき、旅行中に撮影した写真を掲載し、旅の思い出のしおりして残すことができます。"
+                content3="特に「英語が話せない人の海外旅行」向けに事前にメモできる項目が多く設定されています。"
+                isPriority={true}
+              />
+              <Link href="./memorybook/">
+                <Button color="blue" size="normal">
+                  旅のメモリーブックへ
+                </Button>
+              </Link>
             </div>
           </section>
           <Section bgColor="bg-sky-50" name="新着記事">
             <NewArticleTop />
           </Section>
-          <Section bgColor="bg-white" name="カテゴリ">
-            <CategoryTop />
-          </Section>
+          <CategoryList />
         </div>
       </main>
       <Footer />

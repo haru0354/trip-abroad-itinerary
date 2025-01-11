4bcd51c94ee66bbc61db23d0a0453bd8846172e0
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-01-11 18:05:10 +0900

    actionPostImageをfileSaveAndValidate.tsを使用してリファクタリング

--- app/(Blog)/action/actionPostImage.ts
+++ app/(Blog)/action/actionPostImage.ts
@@ -7,9 +7,9 @@ import { z } from "zod";
 import prisma from "@/app/lib/prisma";
 import { supabase } from "@/app/util/supabase";
 import { getPostImage } from "../lib/service/blogServiceUnique";
-import { fileSaveBlogUtils } from "@/app/lib/image-file-save/fileSaveUtils";
-import { validateExtensionAndMineType } from "@/app/lib/image-file-save/validateExtensionAndMineType";
 import { checkUserRole } from "@/app/lib/checkUserRole";
+import { validateSchema } from "@/app/(memorybook)/memorybook/lib/validate/validateSchema";
+import { fileSaveAndValidate } from "@/app/lib/image-file-save/fileSaveAndValidate";
 
 const { unlink } = fsPromises;
 
@@ -42,45 +42,41 @@ export const addPostImage = async (state: FormState, data: FormData) => {
 
   const image = data.get("image") as File;
   const altText = data.get("altText") as string;
-  const validatedFields = schema.safeParse({
+
+  const validateDate = {
     image,
     altText,
-  });
+  };
 
-  if (!validatedFields.success) {
-    const errors = {
-      errors: validatedFields.error.flatten().fieldErrors,
-    };
-    console.log(errors);
-    return errors;
+  const validated = validateSchema(schema, validateDate);
+
+  if (!validated.success) {
+    console.log(validated.errors);
+    return validated.errors;
   }
 
   try {
-    const isValidFile = await validateExtensionAndMineType(image);
+    const result = await fileSaveAndValidate(image, altText);
 
-    if (!isValidFile) {
-      const errors = {
-        errors: {
-          image: [
-            "画像ファイルが無効です。有効な画像ファイルを選択してください。",
-          ],
+    if (result.result) {
+      await prisma.postImage.create({
+        data: {
+          name: result.fileName,
+          url: result.fileUrl,
+          altText,
         },
-      };
-      console.log(errors);
-      return errors;
+      });
+      revalidatePath(`/dashboard/image`);
+      console.log("画像の追加に成功しました");
+    } else {
+      if (result.errors) {
+        console.error("画像のバリデーションエラー:", result.errors);
+        return result.errors;
+      } else if (result.message) {
+        console.error("画像保存時にエラーが発生しました:", result.message);
+        return result.message;
+      }
     }
-
-    const { fileUrl, fileName } = await fileSaveBlogUtils(image);
-
-    await prisma.postImage.create({
-      data: {
-        name: fileName,
-        url: fileUrl,
-        altText,
-      },
-    });
-    revalidatePath(`/dashboard/image`);
-    console.log("画像の追加に成功しました");
   } catch (error) {
     console.error("画像を追加する際にエラーが発生しました");
     return { message: "画像を追加する際にエラーが発生しました" };
@@ -145,16 +141,15 @@ export const updatePostImage = async (
   const image = data.get("image") as File;
   const altText = data.get("altText") as string;
 
-  const validatedFields = updateSchema.safeParse({
+  const validateDate = {
     altText,
-  });
+  };
 
-  if (!validatedFields.success) {
-    const errors = {
-      errors: validatedFields.error.flatten().fieldErrors,
-    };
-    console.log(errors);
-    return errors;
+  const validated = validateSchema(updateSchema, validateDate);
+
+  if (!validated.success) {
+    console.log(validated.errors);
+    return validated.errors;
   }
 
   // altTextのみが変更された場合は、altTextのみを更新
@@ -191,19 +186,29 @@ export const updatePostImage = async (
     }
 
     try {
-      const { fileUrl, fileName } = await fileSaveBlogUtils(image);
+      const result = await fileSaveAndValidate(image, altText);
 
-      await prisma.postImage.update({
-        where: {
-          id,
-        },
-        data: {
-          name: fileName,
-          url: fileUrl,
-          altText,
-        },
-      });
-      revalidatePath(`/dashboard/image`);
+      if (result.result) {
+        await prisma.postImage.update({
+          where: {
+            id,
+          },
+          data: {
+            name: result.fileName,
+            url: result.fileUrl,
+            altText,
+          },
+        });
+        revalidatePath(`/dashboard/image`);
+      } else {
+        if (result.errors) {
+          console.error("画像のバリデーションエラー:", result.errors);
+          return result.errors;
+        } else if (result.message) {
+          console.error("画像保存時にエラーが発生しました:", result.message);
+          return result.message;
+        }
+      }
     } catch (error) {
       console.error("画像を編集する際にエラーが発生しました");
       return { message: "画像を編集する際にエラーが発生しました" };

c47cfb42820284299ec58b0b51c61f98e57b85f3
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-01-13 16:08:26 +0900

    サイトマップページが公開記事のみ表示されるよう変更

--- app/(Blog)/(one-column-page)/sitemaps/page.tsx
+++ app/(Blog)/(one-column-page)/sitemaps/page.tsx
@@ -18,13 +18,16 @@ const page = async () => {
           <Link href="/">{siteTItle}</Link>
         </p>
         {categories.map((category) => {
+          const sortedDraftTruePosts = category.posts
+            .filter((post) => post.draft === true)
+            .sort((a, b) => b.id - a.id);
           return (
             <div className="px-8" key={category.id}>
               <ul className="text-[#2a7bdf]">
                 <li className="my-4 font-semibold">
                   <Link href={`/${category.slug}`}>{category.name}</Link>
                 </li>
-                {category.posts.map((post) => (
+                {sortedDraftTruePosts.map((post) => (
                   <li key={post.id} className="my-4 ml-4 list-disc list-inside">
                     <Link href={`/${category.slug}/${post.slug}`}>
                       {post.title}

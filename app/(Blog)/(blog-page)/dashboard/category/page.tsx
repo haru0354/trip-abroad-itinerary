c4bc394ae028c7c45469dd877b7084795953fc93
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-01-15 20:25:09 +0900

    カテゴリ一覧ページに新規カテゴリページへのボタンリンクを追加

--- app/(Blog)/(blog-page)/dashboard/category/page.tsx
+++ app/(Blog)/(blog-page)/dashboard/category/page.tsx
@@ -1,9 +1,14 @@
+import Link from "next/link";
 import ListCategory from "@/app/(blog)/components/dashboard/list/ListCategory";
+import Button from "@/app/components/ui/Button";
 
 const page = () => {
   return (
     <>
       <ListCategory />
+      <Button color="blue" size="normal" className="my-6">
+        <Link href="/dashboard/category/new-category">カテゴリの追加へ</Link>
+      </Button>
     </>
   );
 };

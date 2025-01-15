f9e15eb8fa990db51699b3879f33fdadaef8e604
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-01-15 20:38:30 +0900

    投稿一覧ページから新規投稿ページへのボタンリンクの追加

--- app/(Blog)/(blog-page)/dashboard/post/page.tsx
+++ app/(Blog)/(blog-page)/dashboard/post/page.tsx
@@ -6,9 +6,14 @@ const page = () => {
   return (
     <>
       <ListDraftTruePosts />
-      <Button color="blue" size="normal" className="my-6">
-        <Link href="/dashboard/post/private-post">未公開記事に切り替え</Link>
-      </Button>
+      <div className="flex flex-wrap items-center justify-center mx-auto max-w-[600px]">
+        <Button color="blue" size="normal" className="my-6">
+          <Link href="/dashboard/post/new-post">新規記事の追加へ</Link>
+        </Button>
+        <Button color="gray" size="normal" className="my-6">
+          <Link href="/dashboard/post/private-post">未公開記事一覧へ</Link>
+        </Button>
+      </div>
     </>
   );
 };

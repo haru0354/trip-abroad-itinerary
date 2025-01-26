32c5020ff4bd9af65ace9295e8f16ddbab43e8a4
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-01-26 17:57:38 +0900

    関連記事が無い場合は関連記事の部分を表示しないよう変更

--- app/(Blog)/components/content-area/related-articles/RelatedArticles.tsx
+++ app/(Blog)/components/content-area/related-articles/RelatedArticles.tsx
@@ -25,13 +25,13 @@ const RelatedArticles: React.FC<RelatedArticlesProps> = async ({
   categorySlug,
   categoryName,
 }) => {
+  if (articles.length === 0) {
+    return null;
+  }
+
   return (
     <>
-      {categoryName ? (
-        <h2>{categoryName}の記事一覧</h2>
-      ) : (
-        <h2>関連記事</h2>
-      )}
+      {categoryName ? <h2>{categoryName}の記事一覧</h2> : <h2>関連記事</h2>}
       {articles.map((article) => {
         return (
           <Link href={`/${categorySlug}/${article.slug}`} key={article.id}>

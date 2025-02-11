844fffb2aae6d2817e80ecc7d3b718fda03494e9
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-02-11 11:15:24 +0900

    アカウント登録をフロント側でもバリデーションできるよう変更

--- app/(memorybook)/memorybook/components/ui/auth/SignupModal.tsx
+++ app/(memorybook)/memorybook/components/ui/auth/SignupModal.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { signIn } from "next-auth/react";
+import { useForm, SubmitHandler } from "react-hook-form";
 import axios from "axios";
 import toast from "react-hot-toast";
 
@@ -10,6 +11,8 @@ import { useModal } from "@/app/hooks/useModal";
 import Modal from "@/app/components/ui/modal/Modal";
 import Input from "@/app/components/ui/form/Input";
 import Button from "@/app/components/ui/button/Button";
+
+import type { UserFormType } from "../../../types/formType";
 
 type SignupModalProps = {
   textButton?: boolean;
@@ -19,29 +22,25 @@ const SignupModal: React.FC<SignupModalProps> = ({ textButton = false }) => {
   const router = useRouter();
   const { closeModal } = useModal();
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<UserFormType>({
+    mode: "onBlur",
+  });
+
   const [errorMessage, setErrorMessage] = useState<string>("");
 
-  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    const name = formData.get("name");
-    const email = formData.get("email");
-    const password = formData.get("password");
-
+  const onSubmit: SubmitHandler<UserFormType> = async (data) => {
     try {
-      const res = await axios.post("/api/signup", {
-        name,
-        email,
-        password,
-      });
+      const res = await axios.post("/api/signup", data);
 
       if (res.status === 200) {
         toast.success("アカウントを作成しました！");
 
         await signIn("itinerary", {
-          name,
-          email,
-          password,
+          ...data,
           redirect: false,
         });
       }
@@ -63,22 +62,32 @@ const SignupModal: React.FC<SignupModalProps> = ({ textButton = false }) => {
       <p className="text-center border-b pb-4 border-itinerary-borderGray font-semibold">
         アカウント作成
       </p>
-      <form onSubmit={onSubmit} className="w-full py-3">
+      <form onSubmit={handleSubmit(onSubmit)} className="w-full py-3">
         <Input
           label="ニックネーム"
           name="name"
-          placeholder="メモの見出しを記載しましょう。"
+          placeholder="ニックネームを記載してください。"
+          register={register}
+          required
+          error={errors.name?.message}
         />
         <Input
+          type="email"
           label="メールアドレス"
           name="email"
-          placeholder="メモの見出しを記載しましょう。"
+          placeholder="メールアドレスを記載してください。。"
+          register={register}
+          required
+          error={errors.email?.message}
         />
         <Input
           type="password"
           label="パスワード"
           name="password"
-          placeholder="メモの見出しを記載しましょう。"
+          placeholder="パスワードを記載してください。。"
+          register={register}
+          required
+          error={errors.password?.message}
         />
         {errorMessage && <p className="text-red-500">{errorMessage}</p>}
         <Button color="blue" size="normal" className="rounded mt-4">

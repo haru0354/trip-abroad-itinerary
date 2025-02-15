ac5facdb314e370a7ef4acc039e662368d094bdd
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-02-15 18:15:39 +0900

    HamburgerMenuを作成

--- app/(memorybook)/memorybook/components/layout/DashboardSideMenu.tsx
+++ app/(memorybook)/memorybook/components/layout/DashboardSideMenu.tsx
@@ -1,16 +1,11 @@
 "use client";
 
-import { useState } from "react";
 import Link from "next/link";
 import { signOut } from "next-auth/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faPlaneDeparture,
-  faHouse,
-  faBars,
-  faXmark,
-} from "@fortawesome/free-solid-svg-icons";
+import { faPlaneDeparture, faHouse } from "@fortawesome/free-solid-svg-icons";
 import Button from "@/app/components/ui/button/Button";
+import HamburgerMenu from "./side-menu/HamburgerMenu";
 
 type DashboardSideMenuProps = {
   trips: Trips[] | undefined;
@@ -22,12 +17,6 @@ type Trips = {
 };
 
 const DashboardSideMenu: React.FC<DashboardSideMenuProps> = ({ trips }) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
-
   const handleLogout = async () => {
     await signOut({ callbackUrl: "/memorybook" });
   };
@@ -93,69 +82,7 @@ const DashboardSideMenu: React.FC<DashboardSideMenuProps> = ({ trips }) => {
           </Button>
         </nav>
       </div>
-      {/* ハンバーガーメニュー */}
-      <div>
-        <button
-          className="fixed top-0 left-0 block sm:hidden text-white p-2 w-12 h-12 ml-[2px] border rounded bg-gray-700 border-itinerary-borderBlack z-10"
-          onClick={toggleMenu}
-        >
-          <FontAwesomeIcon
-            icon={faBars}
-            style={{ width: "14px", height: "16px" }}
-          />
-        </button>
-        {isOpen && (
-          <>
-            <button
-              className="fixed top-0 left-0 block sm:hidden text-white p-2 w-12 h-12 ml-[2px] border rounded bg-gray-700 border-itinerary-borderBlack z-10"
-              onClick={toggleMenu}
-            >
-              <FontAwesomeIcon
-                icon={faXmark}
-                style={{ width: "14px", height: "16px" }}
-              />
-            </button>
-            <div className="fixed top-0 left-0 w-full h-screen bg-gray-500 px-10 pt-12">
-              <p className="py-2 border-b text-white">ダッシュボード</p>
-              <ul className="text-white">
-                <Link href="/memorybook">
-                  <li className="py-2 mx-4">TOPページ</li>
-                </Link>
-                <Link href="/memorybook/dashboard/">
-                  <li className="py-2 mx-4" onClick={toggleMenu}>
-                    ダッシュボード
-                  </li>
-                </Link>
-                <Link href="/memorybook/dashboard/profile">
-                  <li className="py-2 mx-4" onClick={toggleMenu}>
-                    プロフィール
-                  </li>
-                </Link>
-              </ul>
-              <p className="py-2 border-b mt-3 text-white">作成した旅行</p>
-              {trips?.map((trip) => {
-                return (
-                  <ul key={trip.id} className="text-white">
-                    <Link href={`/memorybook/${trip.id}/itinerary`}>
-                      <li key={trip.id} className="py-2 mx-4">
-                        {trip.name}
-                      </li>
-                    </Link>
-                  </ul>
-                );
-              })}
-              <Button
-                onClick={handleLogout}
-                color="white"
-                size="normal"
-                className="rounded mt-4"
-              >
-                ログアウト
-              </Button>
-            </div>
-          </>
-        )}
-      </div>
+      <HamburgerMenu trips={trips} handleLogout={handleLogout} />
     </>
   );
 };
---
+++ app/(memorybook)/memorybook/components/layout/side-menu/HamburgerMenu.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
+
+import Button from "@/app/components/ui/button/Button";
+
+type HamburgerMenuProps = {
+  trips: Trips[] | undefined;
+  handleLogout: () => Promise<void>;
+};
+
+type Trips = {
+  id: number;
+  name: string;
+};
+
+const HamburgerMenu: React.FC<HamburgerMenuProps> = ({
+  trips,
+  handleLogout,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.classList.add("overflow-hidden");
+    } else {
+      document.body.classList.remove("overflow-hidden");
+    }
+
+    return () => {
+      document.body.classList.remove("overflow-hidden");
+    };
+  }, [isOpen]);
+
+  return (
+    <>
+      <button
+        className="fixed top-0 left-0 block sm:hidden text-white p-2 w-12 h-12 ml-[2px] border rounded bg-gray-700 border-itinerary-borderBlack z-10"
+        onClick={toggleMenu}
+      >
+        <FontAwesomeIcon
+          icon={isOpen ? faXmark : faBars}
+          style={{ width: "14px", height: "16px" }}
+        />
+      </button>
+      {isOpen && (
+        <>
+          <div className="fixed top-0 left-0 w-full h-screen bg-gray-500 px-10 pt-12 overflow-y-auto">
+            <p className="py-2 border-b text-white">ダッシュボード</p>
+            <ul className="text-white">
+              <Link href="/memorybook">
+                <li className="py-2 mx-4">TOPページ</li>
+              </Link>
+              <Link href="/memorybook/dashboard/">
+                <li className="py-2 mx-4" onClick={toggleMenu}>
+                  ダッシュボード
+                </li>
+              </Link>
+              <Link href="/memorybook/dashboard/profile">
+                <li className="py-2 mx-4" onClick={toggleMenu}>
+                  プロフィール
+                </li>
+              </Link>
+            </ul>
+            <p className="py-2 border-b mt-3 text-white">作成した旅行</p>
+            {trips?.map((trip) => {
+              return (
+                <ul key={trip.id} className="text-white">
+                  <Link href={`/memorybook/${trip.id}/itinerary`}>
+                    <li key={trip.id} className="py-2 mx-4">
+                      {trip.name}
+                    </li>
+                  </Link>
+                </ul>
+              );
+            })}
+            <Button
+              onClick={handleLogout}
+              color="white"
+              size="normal"
+              className="rounded mt-4"
+            >
+              ログアウト
+            </Button>
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+export default HamburgerMenu;

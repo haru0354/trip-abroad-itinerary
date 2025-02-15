9f33103a7a00a51a6b3492f0532815167e7b2022
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-02-15 17:12:38 +0900

    FormLayoutのデザインを変更し一部propsも変更

--- app/(memorybook)/memorybook/components/layout/FormLayout.tsx
+++ app/(memorybook)/memorybook/components/layout/FormLayout.tsx
@@ -1,18 +1,35 @@
+import { FormEventHandler } from "react";
+import Button from "@/app/components/ui/button/Button";
+
 type FormLayoutProps = {
-  children: React.ReactNode;
+  formTitle: string;
+  buttonName: string;
+  onSubmit: FormEventHandler<HTMLFormElement>;
   maxWidth?: string;
+  children: React.ReactNode;
 };
 
 const FormLayout: React.FC<FormLayoutProps> = ({
-  children,
+  formTitle,
+  buttonName,
+  onSubmit,
   maxWidth = "max-w-[620px]",
+  children,
 }) => {
   return (
     <div className="flex items-center justify-center">
       <div
         className={`w-full py-4 px-6 border rounded border-itinerary-borderGray  bg-white ${maxWidth} `}
       >
-        {children}
+        <p className="text-center border-b pb-4 border-itinerary-borderGray font-semibold">
+          {formTitle}
+        </p>
+        <form onSubmit={onSubmit} className="w-full py-3">
+          {children}
+          <Button color="blue" size="normal" className="rounded mt-4">
+            {buttonName}
+          </Button>
+        </form>
       </div>
     </div>
   );

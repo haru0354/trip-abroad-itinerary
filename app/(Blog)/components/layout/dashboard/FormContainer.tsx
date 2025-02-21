70c22c863d97b5aec161c558961a69e79ab732db
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-02-21 13:17:45 +0900

    FormContainerをボタンやformの部分の記述も追加

--- app/(Blog)/components/layout/dashboard/FormContainer.tsx
+++ app/(Blog)/components/layout/dashboard/FormContainer.tsx
@@ -1,18 +1,36 @@
+import Button from "@/app/components/ui/button/Button";
+import { FormEventHandler } from "react";
+
 type FormContainerProps = {
   children: React.ReactNode;
+  buttonName: string;
   maxWidth?: string;
+  onSubmit?: FormEventHandler<HTMLFormElement>;
+  action?: (
+    data: FormData
+  ) => Promise<{ message?: undefined } | { message: string }>;
 };
 
 const FormContainer: React.FC<FormContainerProps> = ({
   children,
+  buttonName,
   maxWidth = "max-w-full",
+  action,
+  onSubmit,
 }) => {
+  const formProps = action ? { action } : onSubmit ? { onSubmit } : {};
+
   return (
     <div className="flex items-center justify-center">
       <div
         className={`w-full py-4 px-6 border rounded border-blog-borderGray bg-white ${maxWidth}`}
       >
-        {children}
+        <form className="w-full py-3" {...formProps}>
+          {children}
+          <Button color="blue" size="normal" className="rounded mt-4">
+            {buttonName}
+          </Button>
+        </form>
       </div>
     </div>
   );

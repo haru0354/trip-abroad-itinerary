072a3e63bbbffcea650168b3ed61a241e4a65acf
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-02-22 13:14:10 +0900

    useClientが最下層になるようHeadeのrMenuの記述をMenuItemに移行

--- app/(memorybook)/memorybook/components/ui/menu/MenuItem.tsx
+++ app/(memorybook)/memorybook/components/ui/menu/MenuItem.tsx
@@ -1,12 +1,42 @@
 "use client";
 
-type MenuItemProps = {
-  onClick: () => void;
-  label: string;
-};
+import { useState } from "react";
+import { signOut } from "next-auth/react";
+import Image from "next/image";
+import Link from "next/link";
 
-const MenuItem: React.FC<MenuItemProps> = ({ onClick, label }) => {
-  return <div onClick={onClick}>{label}</div>;
+const MenuItem = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const toggleOpen = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  const handleLogout = async () => {
+    await signOut({ callbackUrl: "/memorybook" });
+    toggleOpen();
+  };
+
+  return (
+    <div style={{ position: "relative" }}>
+      <div onClick={toggleOpen} className="cursor-pointer">
+        <Image src="/user_logo01.png" alt="アバター" width={50} height={50} />
+      </div>
+      {isOpen && (
+        <ul className="absolute right-0 mt-2 w-40 p-2 bg-white overflow-hidden shadow-lg z-10 text-base">
+          <li className="cursor-pointer p-2 transition duration-300 hover:bg-itinerary-hoverBlue">
+            <Link href="/memorybook/dashboard">管理画面</Link>
+          </li>
+          <li
+            onClick={handleLogout}
+            className="cursor-pointer p-2 transition duration-300 hover:bg-itinerary-hoverBlue"
+          >
+            ログアウト
+          </li>
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default MenuItem;

8f9c4a2d455d310bb8c15b0631ccd68884fcd06f
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-02-24 12:43:00 +0900

    InputDateAndTimeがバリデーションエラー時と通常時でborderの色が変更するよう編集

--- app/components/ui/form/InputDateAndTime.tsx
+++ app/components/ui/form/InputDateAndTime.tsx
@@ -15,6 +15,8 @@ const InputDateAndTime: React.FC<InputDateAndTimeProps> = ({
   required,
   error,
 }) => {
+  const borderColor = error ? "border-red-500" : "border-gray-300";
+
   return (
     <>
       <label className="block mb-1 mt-4 text-sm font-bold" htmlFor={label}>
@@ -28,6 +30,7 @@ const InputDateAndTime: React.FC<InputDateAndTimeProps> = ({
         {...register(name, {
           required: required && `${label}の入力は必須です。`,
         })}
+        className={`w-full py-2 px-3 shadow border rounded ${borderColor} `}
       />
       {error && <p className="text-red-500 text-sm my-2">{error}</p>}
     </>

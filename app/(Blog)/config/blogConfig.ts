479a23af613c879bb70d7349fcad9758d3627871
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-02-24 12:12:22 +0900

    環境変数のblogのSiteUrlをファイルでexportし読み込むよう変更

--- app/(Blog)/config/blogConfig.ts
+++ app/(Blog)/config/blogConfig.ts
@@ -1,3 +1,4 @@
+export const siteURL = process.env.NEXT_PUBLIC_URL;
 export const blogBrandTitle = process.env.NEXT_PUBLIC_WEBSITE_BRAND_TITLE;
 export const siteTitle = process.env.NEXT_PUBLIC_WEBSITE_TITLE;
 export const siteDescription = process.env.NEXT_PUBLIC_WEBSITE_DESCRIPTION;

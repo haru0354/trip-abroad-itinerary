61b5f76ab65dc9fb6e16920128327840ea0ec160
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-02-24 12:19:51 +0900

    環境変数のitineraryのsiteDescriptionをファイルでexportし読み込むよう変更

--- app/(memorybook)/memorybook/config/itineraryConfig.ts
+++ app/(memorybook)/memorybook/config/itineraryConfig.ts
@@ -1,2 +1,3 @@
 export const itineraryBrandTitle = process.env.NEXT_PUBLIC_ITINERARY_BRAND_TITLE;
 export const itinerarySiteTItle = process.env.NEXT_PUBLIC_ITINERARY_TITLE;
+export const itinerarySiteDescription = process.env.NEXT_PUBLIC_ITINERARY_DESCRIPTION;
--- app/(memorybook)/memorybook/layout.tsx
+++ app/(memorybook)/memorybook/layout.tsx
@@ -3,20 +3,18 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 
 import { ModalProvider } from "@/app/context/ModalContext";
-import { itineraryBrandTitle, itinerarySiteTItle } from "./config/itineraryConfig";
+import { itineraryBrandTitle, itinerarySiteDescription, itinerarySiteTItle } from "./config/itineraryConfig";
 import AuthContext from "../../context/AuthContext";
 import ToastContext from "../../context/ToastContext";
 
 config.autoAddCss = false;
-
-const siteDescription = process.env.NEXT_PUBLIC_ITINERARY_DESCRIPTION;
 
 export const metadata: Metadata = {
   title: {
     default: `${itinerarySiteTItle}`,
     template: `%s | ${itineraryBrandTitle}`,
   },
-  description: `${siteDescription}`,
+  description: `${itinerarySiteDescription}`,
 };
 
 export default async function RootLayout({

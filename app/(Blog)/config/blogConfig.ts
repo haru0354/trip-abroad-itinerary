094d9024dc3e9c8d0e5d3ae5bb9af4334e26a515
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-07-11 14:37:19 +0900

    旅程表アプリの設定を環境変数からitineraryConfigへ変更

--- app/(Blog)/config/blogConfig.ts
+++ app/(Blog)/config/blogConfig.ts
@@ -1,7 +1,9 @@
-export const siteURL = "https://www.my-travel-memory.com/"
-export const blogBrandTitle = "トラベルメモリー"
-export const blogTitle = "英語なしで最高の海外旅行の思い出を作る「トラベルメモリー」"
-export const blogDescription = "海外旅行は記憶に残る最高の思い出になります。そのためにも必要となるのが旅行前の準備と当日の計画をしておくことです。特に英語が話せない人には必要なことでもあります。「トラベルメモリー」では初めての海外旅行や英語が話せない人向けに旅行計画の準備を紹介してます。"
+export const siteURL = "https://www.my-travel-memory.com/";
+export const blogBrandTitle = "トラベルメモリー";
+export const blogTitle =
+  "英語なしで最高の海外旅行の思い出を作る「トラベルメモリー」";
+export const blogDescription =
+  "海外旅行は記憶に残る最高の思い出になります。そのためにも必要となるのが旅行前の準備と当日の計画をしておくことです。特に英語が話せない人には必要なことでもあります。「トラベルメモリー」では初めての海外旅行や英語が話せない人向けに旅行計画の準備を紹介してます。";
 
 // trueと記載の場合のみTOPヘッダーイメージ画像を表示
-export const headerImageConfig = true 
+export const headerImageConfig = true;
--- app/(memorybook)/memorybook/config/itineraryConfig.ts
+++ app/(memorybook)/memorybook/config/itineraryConfig.ts
@@ -1,3 +1,5 @@
-export const itineraryBrandTitle = process.env.NEXT_PUBLIC_ITINERARY_BRAND_TITLE;
-export const itinerarySiteTItle = process.env.NEXT_PUBLIC_ITINERARY_TITLE;
-export const itinerarySiteDescription = process.env.NEXT_PUBLIC_ITINERARY_DESCRIPTION;
+export const itineraryBrandTitle = "旅のメモリーブック";
+export const itinerarySiteTItle =
+  "国内旅行・海外旅行の旅程表作成しおりアプリ「旅のメモリーブック」";
+export const itinerarySiteDescription =
+  "国内旅行・海外の旅行の旅程表を簡単に作成できるしおりアプリが「旅のメモリーブック」です。PC・スマホ・タブレットに対応しており、無料で利用が可能です。また、SNSの共有・同行者の友人・社員旅行など複数の人と旅程が共有可能。写真をアップロードすれば旅の思い出を作れるようになります";

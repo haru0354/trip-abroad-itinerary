ba668981179d322ed415c1219aa62eeb92cfbcde
haru0354 <155523058+haru0354@users.noreply.github.com>
2025-07-25 10:58:58 +0900

    sitemapが動的に生成されるようへんこう

---
+++ app/api/sitemap/route.ts
@@ -0,0 +1,40 @@
+import {
+  getPosts,
+  getCategories,
+} from "@/app/(blog)/lib/service/blogServiceMany";
+import { siteURL } from "@/app/(blog)/config/blogConfig";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const posts = await getPosts("categoryAndPostImage");
+  const categories = await getCategories();
+
+  const urls = [
+    `${siteURL}`,
+    `${siteURL}/privacypolicy`,
+    `${siteURL}/memorybook`,
+    ...posts.map(
+      (post) => `${siteURL}/${post.category?.slug ?? "unknown"}/${post.slug}`
+    ),
+    ...categories.map((category) => `${siteURL}/${category.slug}`),
+  ];
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  ${urls
+    .map(
+      (url) => `
+    <url>
+      <loc>${url}</loc>
+      <lastmod>${new Date().toISOString()}</lastmod>
+    </url>`
+    )
+    .join("")}
+</urlset>`;
+
+  return new NextResponse(xml, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
